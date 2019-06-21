bf38e42c6a013e87579cd84dbf0115f09a42ef71
Bryan Beard <bryanbeard@Bryans-MBP.fios-router.home>
2019-06-21 13:18:19 -0700

    added popup and functionality to set history range in popup

--- backgroundInitialize.js
+++ backgroundInitialize.js
@@ -1,7 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ history: 7 }, () => {
+    chrome.storage.sync.set({ history: 8 }, () => {
         console.log(`Current history set to 7 days.`);
     });
 });
