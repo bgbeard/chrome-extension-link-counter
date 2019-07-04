93712c970fed1b9978885f9cd6dd1f5bf05322a0
Bryan <beardbryan@gmail.com>
2019-07-04 13:52:44 -0700

    delay mode

--- backgrounds/main.js
+++ backgrounds/main.js
@@ -2,7 +2,13 @@
 
 chrome.history.onVisited.addListener((historyItem) => {
     let hostname = new URL(historyItem.url).hostname;
-    chrome.storage.sync.get(['history'], (data) => {
+    chrome.storage.sync.get(['history', 'delayMode'], (data) => {
+        if (data.delayMode) {
+            chrome.tabs.executeScript({
+                file: '/backgrounds/delay.js',
+                runAt: 'document_start'
+            })
+        }
         let searchQuery = { text: hostname, startTime: data.history, maxResults: 1000 };
         chrome.history.search(searchQuery, (historyItems) => {
             chrome.browserAction.setBadgeText({ text: historyItems.length.toString() });
